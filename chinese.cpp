a05e8f7dc5ef090ac7776832423b90aec60dfc7b
XiaY-Summer <107897606+XiaY-Summer@users.noreply.github.com>
2022-12-24 11:24:02 +0800

    chinese.cpp

---
+++ chinese.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <stdlib.h>
+#include <fstream>
+using namespace std;
+int main(){
+	system("pacman -Sy wqy-bitmapfont --noconfirm");
+	system("pacman -Sy wqy-microhei --noconfirm");
+	system("pacman -Sy wqy-zenhei --noconfirm");
+
+}
