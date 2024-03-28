a0ffbf30b79dfd89dc9a640216935e9d5308332a
XiaY-Summer <107897606+XiaY-Summer@users.noreply.github.com>
2024-03-29 00:55:33 +0800

    Update update&install.cpp

--- update&install.cpp
+++ update&install.cpp
@@ -1,14 +1,8 @@
-#include <iostream>
-#include <stdlib.h>
-#include <fstream>
-using namespace std;
-int main(){
-	system("sudo sed -i '$a\\[archlinuxcn]' /etc/pacman.conf");
-	system("sudo sed -i '$a\\SigLevel = Optional TrustedOnly' /etc/pacman.conf");
-	system("sudo sed -i '$a\\Server = https://mirrors.ustc.edu.cn/archlinuxcn/$arch' /etc/pacman.conf");
-	system("sudo pacman -Syyu --noconfirm");
-	system("sudo pacman -S archlinuxcn-keyring");
-	system("sudo pacman -S fcitx5-im fcitx5-chinese-addons --noconfirm");
-	system("sudo pacman -S yay --noconfirm");
-
-}
+sudo sed -i '$a\\[archlinuxcn]' /etc/pacman.conf
+sudo sed -i '$a\\SigLevel = Optional TrustedOnly' /etc/pacman.conf
+sudo sed -i '$a\\Server = https://mirrors.ustc.edu.cn/archlinuxcn/$arch' /etc/pacman.conf
+sudo pacman -Syyu --noconfirm
+sudo pacman-key --lsign-key "farseerfc@archlinux.org"
+sudo pacman -S archlinuxcn-keyring --noconfirm
+sudo pacman -S fcitx5-im fcitx5-chinese-addons --noconfirm
+sudo pacman -S yay --noconfirm
