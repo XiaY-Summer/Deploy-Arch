e6ca8d87f4075cc5dd5b2dba3a06a1baf42ae409
XiaY-Summer <107897606+XiaY-Summer@users.noreply.github.com>
2024-03-29 01:15:00 +0800

    Update Fcitx5-Theme.sh

--- Fcitx5-Theme.sh
+++ Fcitx5-Theme.sh
@@ -3,3 +3,5 @@
 git clone https://github.com/catppuccin/fcitx5.git
 mkdir -p ~/.local/share/fcitx5/themes/
 cp -r ./fcitx5/src/* ~/.local/share/fcitx5/themes
+sed -i 'Theme=catppuccin-macchiato' ~/.config/fcitx5/conf/classicui.conf
+fcitx5 -r
