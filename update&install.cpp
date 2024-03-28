433ac37dd685bbf7e3b6a2ed6ac9289925efc19e
XiaY-Summer <107897606+XiaY-Summer@users.noreply.github.com>
2024-03-29 00:49:53 +0800

    Update update&install.cpp

--- update&install.cpp
+++ update&install.cpp
@@ -7,28 +7,8 @@ int main(){
 	system("sudo sed -i '$a\\SigLevel = Optional TrustedOnly' /etc/pacman.conf");
 	system("sudo sed -i '$a\\Server = https://mirrors.ustc.edu.cn/archlinuxcn/$arch' /etc/pacman.conf");
 	system("sudo pacman -Syyu --noconfirm");
-	system("sudo pacman -Syy --noconfirm");
 	system("sudo pacman -S archlinuxcn-keyring");
-
 	system("sudo pacman -S fcitx5-im fcitx5-chinese-addons --noconfirm");
-	system("sudo pacman -S git --noconfirm");
-	system("git clone https://github.com/thep0y/fcitx5-themes.git");
-	system("mkdir -p ~/.local/share/fcitx5/themes"); 
-	system("cp ./fcitx5-themes/winter ~/.local/share/fcitx5/themes -r");
-	ofstream fout("classicui.conf");
-	fout<<"Vertical Candidate List=False"<<endl;
-	fout<<"PerScreenDPI=True"<<endl;
-	fout<<"Font=\"Smartisan Compact CNS 13\""<<endl;
-	fout<<"Theme=winter";
-	fout.close();
-	system("mkdir -p ~/.config/fcitx5/conf");
-	system("touch ~/.config/fcitx5/conf/classicui.conf");
-	system("cp classicui.conf ~/.config/fcitx5/conf/classicui.conf");
-
 	system("sudo pacman -S yay --noconfirm");
-	system("sudo pacman -S firefox --noconfirm");
-	system("sudo pacman -S fcitx5-pinyin-zhwiki --noconfirm");
-	system("sudo cp -r .xprofile ~");
-	system("sudo cp -r .pam_environment ~");
 
 }
